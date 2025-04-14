aca35eea2c95998095aede8068414193e2c39157
javvidar004 <157633880+javvidar004@users.noreply.github.com>
2025-04-14 23:29:09 +0000

    Add files via upload

---
+++ Parte2/Node/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TablaDatos from './TablaDatos'; // Importa tu componente
+import './App.css'; // Puedes importar estilos aquí si los tienes
+
+function App() {
+  return (
+    <div className="App">
+      {/* Simplemente renderiza tu componente */}
+      <TablaDatos />
+
+      {/* Puedes añadir más contenido aquí si lo necesitas */}
+    </div>
+  );
+}
+
+export default App;
